75e34d23ba1a166d593e711d92603ae933a7476e
SE JIHAD <ijihad639@gmail.com>
2026-02-10 04:04:34 +0600

    e

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,85 +6,90 @@ import Footer from "./component/layout/Footer";
 import Header from "./component/layout/Header";
 import ScrollToTop from "./component/layout/ScrollToTop";
 import ProtectedRoute from "./component/Route/ProtectedRoute";
-import AllBlogs from "./pages/Admin/AllBlogs";
-import AllCategories from "./pages/Admin/AllCategories";
-import AllOrders from "./pages/Admin/AllOrders";
-import AllProducts from "./pages/Admin/AllProducts";
-import UserEmails from "./pages/Admin/UserEmails";
-import ProductDetails from "./pages/ProductDetails/ProductDetails";
-const AdminOrderDetails = lazy(() => import("./pages/Admin/AdminOrderDetails"));
-
-import AllReviews from "./pages/Admin/AllReviews";
-import AllShips from "./pages/Admin/AllShips";
-import AllUsers from "./pages/Admin/AllUsers";
-import Dashboard from "./pages/Admin/Dashboard";
-import Messages from "./pages/Admin/Messages.jsx";
-import NewBlog from "./pages/Admin/NewBlog";
-import NewProduct from "./pages/Admin/NewProduct";
-
-import Reviews from "./pages/Admin/Reviews";
-import UpdateBlog from "./pages/Admin/UpdateBlog";
-import UpdateProduct from "./pages/Admin/UpdateProduct";
-
-import UserDetails from "./pages/Admin/UserDetails";
-import ForgotPassword from "./pages/Auth/ForgotPassword";
-import GoogleSuccess from "./pages/Auth/GoogleSuccess";
-import Login from "./pages/Auth/Login";
-import ResetPassword from "./pages/Auth/ResetPassword";
-import BlogDetails from "./pages/Blogs/BlogDetails";
-import Blogs from "./pages/Blogs/Blogs";
-
-import Cart from "./pages/Cart/Cart";
-import Contact from "./pages/Contact/Contact";
-
-import Home from "./pages/Home/Home";
-import NotFound from "./pages/NotFound/NotFound";
-import Notify from "./pages/Notify/Notify.jsx";
-import MyOrders from "./pages/Orders/MyOrders";
-import OrderDetails from "./pages/Orders/OrderDetails";
 
 // import Checkout from "./pages/Payment/Checkout";
+// without lazy loading
 import { jwtDecode } from "jwt-decode";
 import { useDispatch, useSelector } from "react-redux";
 import { loadUser, logout } from "./actions/userAction";
+import Loader from "./component/layout/Loader/Loader.jsx";
 import NotificationBanner from "./component/layout/NotificationBanner";
-import About from "./pages/About/About";
-import AllBanners from "./pages/Admin/AllBanners";
-import AllBrands from "./pages/Admin/AllBrands";
-import AllCharges from "./pages/Admin/AllCharges";
-import AllCoupons from "./pages/Admin/AllCoupons";
-import AllCustomLogoCharges from "./pages/Admin/AllCustomLogoCharges";
-import AllLogos from "./pages/Admin/AllLogos";
-import AllSubcategories from "./pages/Admin/AllSubcategories";
-import AllSubsubcategories from "./pages/Admin/AllSubsubcategories";
-import AllTypes from "./pages/Admin/AllTypes";
-
 import { ROLE_GROUPS, ROLES } from "./constants/roles.jsx";
-import AllGenders from "./pages/Admin/AllGenders";
-import AllNotifies from "./pages/Admin/AllNotifies.jsx";
-import NotificationManager from "./pages/Admin/NotificationManager";
-import Notifications from "./pages/Notify/Notifications.jsx";
-import Checkout from "./pages/Payment/Checkout";
-import PaymentCancel from "./pages/Payment/PaymentCancel";
-import PaymentFail from "./pages/Payment/PaymentFail";
-import PaymentSuccess from "./pages/Payment/PaymentSuccess";
-import PrivacyPolicy from "./pages/Privacy/PrivacyPolicy";
-import ShippingPolicy from "./pages/ShippingPolicy/ShippingPolicy";
-import Shop from "./pages/Shop/Shop";
-import SuspendedAccount from "./pages/Suspend/SuspendedAccount";
-import TermsConditions from "./pages/Terms/TermsAndConditions";
-import Delete from "./pages/User/Delete";
-import DeletedAccount from "./pages/User/DeletedAccount.jsx";
-import Profile from "./pages/User/Profile";
-import Setting from "./pages/User/Setting";
-import UpdatePassword from "./pages/User/UpdatePassword";
-import UpdateProfile from "./pages/User/UpdateProfile";
 import { connectSocket } from "./utils/socket.js";
-const LazyAdminOrder = () => (
-  <Suspense fallback={<div>Loading...</div>}>
-    <AdminOrderDetails />
-  </Suspense>
+
+const AllBlogs = lazy(() => import("./pages/Admin/AllBlogs"));
+const AllCategories = lazy(() => import("./pages/Admin/AllCategories"));
+const AllOrders = lazy(() => import("./pages/Admin/AllOrders"));
+const AllProducts = lazy(() => import("./pages/Admin/AllProducts"));
+const UserEmails = lazy(() => import("./pages/Admin/UserEmails"));
+const ProductDetails = lazy(
+  () => import("./pages/ProductDetails/ProductDetails"),
 );
+const AdminOrderDetails = lazy(() => import("./pages/Admin/AdminOrderDetails"));
+const AllReviews = lazy(() => import("./pages/Admin/AllReviews"));
+const AllShips = lazy(() => import("./pages/Admin/AllShips"));
+const AllUsers = lazy(() => import("./pages/Admin/AllUsers"));
+const Dashboard = lazy(() => import("./pages/Admin/Dashboard"));
+const Messages = lazy(() => import("./pages/Admin/Messages.jsx"));
+const NewBlog = lazy(() => import("./pages/Admin/NewBlog"));
+const NewProduct = lazy(() => import("./pages/Admin/NewProduct"));
+
+const Reviews = lazy(() => import("./pages/Admin/Reviews"));
+const UpdateBlog = lazy(() => import("./pages/Admin/UpdateBlog"));
+const UpdateProduct = lazy(() => import("./pages/Admin/UpdateProduct"));
+const UserDetails = lazy(() => import("./pages/Admin/UserDetails"));
+const ForgotPassword = lazy(() => import("./pages/Auth/ForgotPassword"));
+const GoogleSuccess = lazy(() => import("./pages/Auth/GoogleSuccess"));
+const Login = lazy(() => import("./pages/Auth/Login"));
+const ResetPassword = lazy(() => import("./pages/Auth/ResetPassword"));
+
+const BlogDetails = lazy(() => import("./pages/Blogs/BlogDetails"));
+const Blogs = lazy(() => import("./pages/Blogs/Blogs"));
+const Cart = lazy(() => import("./pages/Cart/Cart"));
+const Contact = lazy(() => import("./pages/Contact/Contact"));
+const Home = lazy(() => import("./pages/Home/Home"));
+const NotFound = lazy(() => import("./pages/NotFound/NotFound"));
+const Notify = lazy(() => import("./pages/Notify/Notify"));
+const MyOrders = lazy(() => import("./pages/Orders/MyOrders"));
+const OrderDetails = lazy(() => import("./pages/Orders/OrderDetails"));
+const About = lazy(() => import("./pages/About/About"));
+const AllBanners = lazy(() => import("./pages/Admin/AllBanners"));
+const AllBrands = lazy(() => import("./pages/Admin/AllBrands"));
+const AllCharges = lazy(() => import("./pages/Admin/AllCharges"));
+const AllCoupons = lazy(() => import("./pages/Admin/AllCoupons"));
+const AllCustomLogoCharges = lazy(
+  () => import("./pages/Admin/AllCustomLogoCharges"),
+);
+const AllLogos = lazy(() => import("./pages/Admin/AllLogos"));
+const AllSubcategories = lazy(() => import("./pages/Admin/AllSubcategories"));
+const AllSubsubcategories = lazy(
+  () => import("./pages/Admin/AllSubsubcategories"),
+);
+const AllTypes = lazy(() => import("./pages/Admin/AllTypes"));
+const AllGenders = lazy(() => import("./pages/Admin/AllGenders"));
+const AllNotifies = lazy(() => import("./pages/Admin/AllNotifies"));
+const NotificationManager = lazy(
+  () => import("./pages/Admin/NotificationManager"),
+);
+const Notifications = lazy(() => import("./pages/Notify/Notifications"));
+const Checkout = lazy(() => import("./pages/Payment/Checkout"));
+const PaymentCancel = lazy(() => import("./pages/Payment/PaymentCancel"));
+const PaymentFail = lazy(() => import("./pages/Payment/PaymentFail"));
+const PaymentSuccess = lazy(() => import("./pages/Payment/PaymentSuccess"));
+const PrivacyPolicy = lazy(() => import("./pages/Privacy/PrivacyPolicy"));
+const ShippingPolicy = lazy(
+  () => import("./pages/ShippingPolicy/ShippingPolicy"),
+);
+const Shop = lazy(() => import("./pages/Shop/Shop"));
+const SuspendedAccount = lazy(() => import("./pages/Suspend/SuspendedAccount"));
+const TermsConditions = lazy(() => import("./pages/Terms/TermsAndConditions"));
+const Delete = lazy(() => import("./pages/User/Delete"));
+const DeletedAccount = lazy(() => import("./pages/User/DeletedAccount"));
+const Profile = lazy(() => import("./pages/User/Profile"));
+const Setting = lazy(() => import("./pages/User/Setting"));
+const UpdatePassword = lazy(() => import("./pages/User/UpdatePassword"));
+const UpdateProfile = lazy(() => import("./pages/User/UpdateProfile"));
+
 const App = () => {
   const dispatch = useDispatch();
   const location = useLocation();
@@ -159,118 +164,128 @@ const App = () => {
       <Header />
       <ScrollToTop />
       <NotificationBanner />
-      <Routes>
-        {/* public routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/blog/:slug" element={<BlogDetails />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/terms&conditions" element={<TermsConditions />} />
-        <Route path="/shipping-policy" element={<ShippingPolicy />} />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          {/* public routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blog/:slug" element={<BlogDetails />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/terms&conditions" element={<TermsConditions />} />
+          <Route path="/shipping-policy" element={<ShippingPolicy />} />
 
-        <Route path="/:category/:slug" element={<ProductDetails />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/suspended" element={<SuspendedAccount />} />
-        <Route path="/deleted" element={<DeletedAccount />} />
-        <Route path="/google-success" element={<GoogleSuccess />} />
-        <Route path="/facebook-success" element={<GoogleSuccess />} />
-        <Route path="/password/forgot" element={<ForgotPassword />} />
-        <Route path="/password/reset/:token" element={<ResetPassword />} />
-        {/* notfound  */}
-        <Route path="*" element={<NotFound />} />
+          <Route path="/:category/:slug" element={<ProductDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/suspended" element={<SuspendedAccount />} />
+          <Route path="/deleted" element={<DeletedAccount />} />
+          <Route path="/google-success" element={<GoogleSuccess />} />
+          <Route path="/facebook-success" element={<GoogleSuccess />} />
+          <Route path="/password/forgot" element={<ForgotPassword />} />
+          <Route path="/password/reset/:token" element={<ResetPassword />} />
+          {/* notfound  */}
+          <Route path="*" element={<NotFound />} />
 
-        {/* protected routes */}
-        {/* all user routes */}
-        <Route element={<ProtectedRoute roles={ROLE_GROUPS.ALL_USERS} />}>
-          <Route path="/notification/:id" element={<Notify />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/profile/update" element={<UpdateProfile />} />
-          <Route path="/profile/delete" element={<Delete />} />
-          <Route path="/password/update" element={<UpdatePassword />} />
-          <Route path="/profile/setting" element={<Setting />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/orders" element={<MyOrders />} />
-          <Route path="/order/:id" element={<OrderDetails />} />
-          <Route path="/payment-success" element={<PaymentSuccess />} />
-          <Route path="/payment-cancel" element={<PaymentCancel />} />
-          <Route path="/payment-fail" element={<PaymentFail />} />
-        </Route>
+          {/* protected routes */}
+          {/* all user routes */}
+          <Route element={<ProtectedRoute roles={ROLE_GROUPS.ALL_USERS} />}>
+            <Route path="/notification/:id" element={<Notify />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/notifications" element={<Notifications />} />
+            <Route path="/profile/update" element={<UpdateProfile />} />
+            <Route path="/profile/delete" element={<Delete />} />
+            <Route path="/password/update" element={<UpdatePassword />} />
+            <Route path="/profile/setting" element={<Setting />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/orders" element={<MyOrders />} />
+            <Route path="/order/:id" element={<OrderDetails />} />
+            <Route path="/payment-success" element={<PaymentSuccess />} />
+            <Route path="/payment-cancel" element={<PaymentCancel />} />
+            <Route path="/payment-fail" element={<PaymentFail />} />
+          </Route>
 
-        {/* moderator, admin, super-admin route */}
-        <Route
-          element={
-            <ProtectedRoute
-              roles={[...ROLE_GROUPS.MODS_AND_UP, ROLES.USER_ADMIN]}
-            />
-          }
-        >
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/messages" element={<Messages />} />
-        </Route>
-
-        {/* admin and super-admin routes */}
-        <Route element={<ProtectedRoute roles={ROLE_GROUPS.ADMINS_AND_UP} />}>
-          <Route path="/admin/categories" element={<AllCategories />} />
-          <Route path="/admin/logos" element={<AllLogos />} />
-          <Route path="/admin/banners" element={<AllBanners />} />
-          <Route path="/admin/subcategories" element={<AllSubcategories />} />
+          {/* moderator, admin, super-admin route */}
           <Route
-            path="/admin/subsubcategories"
-            element={<AllSubsubcategories />}
-          />
-          <Route path="/admin/brands" element={<AllBrands />} />
-          <Route path="/admin/types" element={<AllTypes />} />
-          <Route path="/admin/genders" element={<AllGenders />} />
-          <Route path="/admin/product/new" element={<NewProduct />} />
-          <Route path="/admin/product/:id" element={<UpdateProduct />} />
-          <Route path="/admin/products" element={<AllProducts />} />
-          <Route path="/admin/reviews/:id" element={<Reviews />} />
-          <Route path="/admin/reviews" element={<AllReviews />} />
-        </Route>
-        {/* admin-user , admin ,super-admin */}
-        <Route
-          element={
-            <ProtectedRoute
-              roles={[...ROLE_GROUPS.ADMINS_AND_UP, ROLES.USER_ADMIN]}
+            element={
+              <ProtectedRoute
+                roles={[...ROLE_GROUPS.MODS_AND_UP, ROLES.USER_ADMIN]}
+              />
+            }
+          >
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/messages" element={<Messages />} />
+          </Route>
+
+          {/* admin and super-admin routes */}
+          <Route element={<ProtectedRoute roles={ROLE_GROUPS.ADMINS_AND_UP} />}>
+            <Route path="/admin/categories" element={<AllCategories />} />
+            <Route path="/admin/logos" element={<AllLogos />} />
+            <Route path="/admin/banners" element={<AllBanners />} />
+            <Route path="/admin/subcategories" element={<AllSubcategories />} />
+            <Route
+              path="/admin/subsubcategories"
+              element={<AllSubsubcategories />}
             />
-          }
-        >
-          <Route path="/admin/blog" element={<NewBlog />} />
-          <Route path="/admin/blogs" element={<AllBlogs />} />
-          <Route path="/admin/blog/:id" element={<UpdateBlog />} />
-        </Route>
-        {/* admin-user and super-admin */}
-        <Route
-          element={
-            <ProtectedRoute roles={[ROLES.SUPER_ADMIN, ROLES.USER_ADMIN]} />
-          }
-        >
-          <Route path="/admin/coupons" element={<AllCoupons />} />
-          <Route path="/admin/emails" element={<UserEmails />} />
-          <Route path="/admin/notifies" element={<AllNotifies />} />
-        </Route>
-        {/* super-admin route */}
-        <Route
-          element={<ProtectedRoute roles={ROLE_GROUPS.SUPER_ADMIN_ONLY} />}
-        >
-          <Route path="/admin/users" element={<AllUsers />} />
-          <Route path="/admin/user/:id" element={<UserDetails />} />
+            <Route path="/admin/brands" element={<AllBrands />} />
+            <Route path="/admin/types" element={<AllTypes />} />
+            <Route path="/admin/genders" element={<AllGenders />} />
+            <Route path="/admin/product/new" element={<NewProduct />} />
+            <Route path="/admin/product/:id" element={<UpdateProduct />} />
+            <Route path="/admin/products" element={<AllProducts />} />
+            <Route path="/admin/reviews/:id" element={<Reviews />} />
+            <Route path="/admin/reviews" element={<AllReviews />} />
+          </Route>
+          {/* admin-user , admin ,super-admin */}
+          <Route
+            element={
+              <ProtectedRoute
+                roles={[...ROLE_GROUPS.ADMINS_AND_UP, ROLES.USER_ADMIN]}
+              />
+            }
+          >
+            <Route path="/admin/blog" element={<NewBlog />} />
+            <Route path="/admin/blogs" element={<AllBlogs />} />
+            <Route path="/admin/blog/:id" element={<UpdateBlog />} />
+          </Route>
+          {/* admin-user and super-admin */}
+          <Route
+            element={
+              <ProtectedRoute roles={[ROLES.SUPER_ADMIN, ROLES.USER_ADMIN]} />
+            }
+          >
+            <Route path="/admin/coupons" element={<AllCoupons />} />
+            <Route path="/admin/emails" element={<UserEmails />} />
+            <Route path="/admin/notifies" element={<AllNotifies />} />
+          </Route>
+          {/* super-admin route */}
+          <Route
+            element={<ProtectedRoute roles={ROLE_GROUPS.SUPER_ADMIN_ONLY} />}
+          >
+            <Route path="/admin/users" element={<AllUsers />} />
+            <Route path="/admin/user/:id" element={<UserDetails />} />
 
-          <Route path="/admin/charges" element={<AllCharges />} />
-          <Route path="/admin/logocharges" element={<AllCustomLogoCharges />} />
-          <Route path="/admin/ships" element={<AllShips />} />
-          <Route path="/admin/orders" element={<AllOrders />} />
+            <Route path="/admin/charges" element={<AllCharges />} />
+            <Route
+              path="/admin/logocharges"
+              element={<AllCustomLogoCharges />}
+            />
+            <Route path="/admin/ships" element={<AllShips />} />
+            <Route path="/admin/orders" element={<AllOrders />} />
 
-          <Route path="/admin/order/:id" element={<LazyAdminOrder />} />
+            <Route path="/admin/order/:id" element={<AdminOrderDetails />} />
 
-          <Route path="/admin/notification" element={<NotificationManager />} />
-        </Route>
-      </Routes>
+            <Route
+              path="/admin/notification"
+              element={<NotificationManager />}
+            />
+          </Route>
+        </Routes>
+        {/* Suspense fallback */}
+      </Suspense>
+
       <Toaster
         position="top-center" // top-middle equivalent
         richColors // colorful success/error/info
