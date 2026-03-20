4213c018141257708b43e1e2efd26b61f297e203
SE JIHAD <ijihad639@gmail.com>
2026-03-20 21:19:49 +0600

    e

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -3,7 +3,7 @@ const ErrorHandler = require("../utils/errorHandler");
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
-
+  console.log(err.message);
   // ❌ Wrong MongoDB ID (CastError)
   if (err.name === "CastError") {
     const message = `Resource not found. Invalid ${err.path}`;
